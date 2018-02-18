632f8aec870afc7d77f302abecff45d03f204980
VasyaInformatica <stepanov@informatica.ru>
2018-02-18 22:38:54 +0300

    init

---
+++ index.js
@@ -0,0 +1,11 @@
+const {sendTelegranMessage}=require("telegramBot")
+
+setInterval(()=>{
+
+
+    sendTelegranMessage({type:'error', message:"У НАС ВСЕ ГОРИИИИТ!Т!!!!"})
+},40000)
+
+setInterval(()=>{
+    
+},20000)
